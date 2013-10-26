a34a6a7a40d47988ce31cb67a6f4c91e6ae28bd3
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-26 16:28:18 -0400

    first commit

---
+++ app/controllers/app.go
@@ -0,0 +1,28 @@
+package controllers
+
+import "github.com/robfig/revel"
+
+type App struct {
+    *revel.Controller
+}
+
+func (c App) Index() revel.Result {
+    pageHeader := "Main page!!"
+    posts := make([]string, 5)
+    posts[0] = "This is the first post!!!"
+    posts[1] = "This is the second post!!!"
+    posts[2] = "This is the third post!!!"
+    posts[3] = "This is the fourth post!!!"
+    posts[4] = "This is the fifth post!!!"
+    return c.Render(posts, pageHeader)
+}
+
+func (c App) Show() revel.Result {
+    pageHeader := "Show page!!"
+    return c.Render(pageHeader)
+}
+
+func (c App) New() revel.Result {
+    pageHeader := "New post!!"
+    return c.Render(pageHeader)
+}
