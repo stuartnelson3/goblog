b10463323f251f321b9732743cb3933fa31832cc
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-26 21:02:59 -0400

    show page pulling posts from db

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,9 +1,25 @@
 package controllers
 
-import "github.com/robfig/revel"
+import (
+    "github.com/robfig/revel"
+    "github.com/coopernurse/gorp"
+    _ "github.com/bmizerany/pq"
+    "database/sql"
+    "log"
+    "os"
+)
+
+var db, err = sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable")
+var dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
 
 type App struct {
     *revel.Controller
+}
+
+type Post struct {
+    Id int64 `db:"id"`
+    Title string `db:"title"`
+    Body string `db:"body"`
 }
 
 func (c App) Index() revel.Result {
@@ -19,16 +35,14 @@ func (c App) Index() revel.Result {
 
 func (c App) Show(id int) revel.Result {
     pageHeader := "Show page!!"
-    posts := make(map[int]string)
-    posts[1] = "This is the first post!!!"
-    posts[2] = "This is the second post!!!"
-    posts[3] = "This is the third post!!!"
-    posts[4] = "This is the fourth post!!!"
-    posts[5] = "This is the fifth post!!!"
-    post, prs := posts[id]
-    if !prs {
+    dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
+    obj, err := dbmap.Get(Post{}, id)
+    // post, err := dbmap.Select("select body from posts where id=$1", id)
+    dbmap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
+    post := obj.(*Post)
+    if err != nil {
         c.Response.Status = 404
-        return c.NotFound("Page not found")
+        return c.NotFound("Not found")
     }
     return c.Render(post, pageHeader)
 }
--- app/init.go
+++ app/init.go
@@ -1,19 +1,42 @@
 package app
 
-import "github.com/robfig/revel"
+import (
+    "github.com/robfig/revel"
+    "github.com/coopernurse/gorp"
+    _ "github.com/bmizerany/pq"
+    "database/sql"
+    "os"
+    "log"
+)
+
+type Post struct {
+    Id int64
+    Title string
+    Body string
+}
 
 func init() {
-	// Filters is the default set of global filters.
-	revel.Filters = []revel.Filter{
-		revel.PanicFilter,             // Recover from panics and display an error page instead.
-		revel.RouterFilter,            // Use the routing table to select the right Action
-		revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
-		revel.ParamsFilter,            // Parse parameters into Controller.Params.
-		revel.SessionFilter,           // Restore and write the session cookie.
-		revel.FlashFilter,             // Restore and write the flash cookie.
-		revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
-		revel.I18nFilter,              // Resolve the requested language
-		revel.InterceptorFilter,       // Run interceptors around the action.
-		revel.ActionInvoker,           // Invoke the action.
-	}
+    db, err := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable")
+
+    if err != nil { panic(err) }
+
+    dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+    dbmap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
+
+    dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
+    dbmap.CreateTablesIfNotExists()
+
+    // Filters is the default set of global filters.
+    revel.Filters = []revel.Filter{
+        revel.PanicFilter,             // Recover from panics and display an error page instead.
+        revel.RouterFilter,            // Use the routing table to select the right Action
+        revel.FilterConfiguringFilter, // A hook for adding or removing per-Action filters.
+        revel.ParamsFilter,            // Parse parameters into Controller.Params.
+        revel.SessionFilter,           // Restore and write the session cookie.
+        revel.FlashFilter,             // Restore and write the flash cookie.
+        revel.ValidationFilter,        // Restore kept validation errors and save new ones from cookie.
+        revel.I18nFilter,              // Resolve the requested language
+        revel.InterceptorFilter,       // Run interceptors around the action.
+        revel.ActionInvoker,           // Invoke the action.
+    }
 }
