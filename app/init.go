916b371135174ccbe322d034c6bd970d6fd70cfc
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-26 21:52:54 -0400

    removed unneeded code from init.go

--- app/init.go
+++ app/init.go
@@ -2,30 +2,9 @@ package app
 
 import (
     "github.com/robfig/revel"
-    "github.com/coopernurse/gorp"
-    _ "github.com/bmizerany/pq"
-    "database/sql"
-    "os"
-    "log"
 )
 
-type Post struct {
-    Id int64
-    Title string
-    Body string
-}
-
 func init() {
-    db, err := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable")
-
-    if err != nil { panic(err) }
-
-    dbmap := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
-    dbmap.TraceOn("[gorp]", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
-
-    dbmap.AddTableWithName(Post{}, "posts").SetKeys(true, "Id")
-    dbmap.CreateTablesIfNotExists()
-
     // Filters is the default set of global filters.
     revel.Filters = []revel.Filter{
         revel.PanicFilter,             // Recover from panics and display an error page instead.
