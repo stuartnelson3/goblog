f0c40cb91d7ee2080041d130744a4aca780be27a
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-27 12:06:01 -0400

    add query tracing, and create tables if they dont exist

--- db/dbsetup.go
+++ db/dbsetup.go
@@ -5,12 +5,17 @@ import (
     _ "github.com/bmizerany/pq"
     "database/sql"
     "blog/app/models"
+    "os"
+    "log"
 )
 
 func DbSetup() (dbmap *gorp.DbMap) {
     db, _ := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable")
     dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+    dbmap.TraceOn("query:", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 
     dbmap.AddTableWithName(models.Post{}, "posts").SetKeys(true, "Id")
+    dbmap.CreateTablesIfNotExists()
+
     return dbmap
 }
