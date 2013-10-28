fd12612f3b02667ddbd2f87ae9a0eb22b93811be
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-28 16:18:28 -0400

    format test code

--- tests/apptest.go
+++ tests/apptest.go
@@ -3,19 +3,19 @@ package tests
 import "github.com/robfig/revel"
 
 type AppTest struct {
-	revel.TestSuite
+    revel.TestSuite
 }
 
 func (t *AppTest) Before() {
-	println("Set up")
+    println("Set up")
 }
 
 func (t AppTest) TestThatIndexPageWorks() {
-	t.Get("/")
-	t.AssertOk()
-	t.AssertContentType("text/html")
+    t.Get("/")
+    t.AssertOk()
+    t.AssertContentType("text/html")
 }
 
 func (t *AppTest) After() {
-	println("Tear down")
+    println("Tear down")
 }
