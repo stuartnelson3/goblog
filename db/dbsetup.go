c797313911ed29a2b46533491d1d4ceffb3ec48e
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-31 22:18:22 -0400

    get pg spec from env variables

--- db/dbsetup.go
+++ db/dbsetup.go
@@ -8,9 +8,16 @@ import (
     "log"
 )
 
+func CreatePgSpec(key string, heroku_env_var string) string {
+    return key+"="+os.Getenv(heroku_env_var)+" "
+}
+
 func DbSetup(m map[string]interface{}) (dbmap *gorp.DbMap) {
-    // db, _ := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable") // dev
-    db, _ := sql.Open("postgres", "user=u6aq2gkmaiir3hrv password=56afebf98a164079989849adc9d589d1 host=bcvpvcjrov5cbmt6.postgresql.clvrcld.net dbname=bcvpvcjrov5cbmt6 sslmode=disable") // "prod"
+    user := CreatePgSpec("user", os.Getenv("HEROKU_POSTGRESQL_USER"))
+    pass := CreatePgSpec("password", os.Getenv("HEROKU_POSTGRESQL_PASS"))
+    dbname := CreatePgSpec("dbname", os.Getenv("HEROKU_POSTGRESQL_DBNAME"))
+    host := CreatePgSpec("host", os.Getenv("HEROKU_POSTGRESQL_HOST"))
+    db, _ := sql.Open("postgres", user+pass+host+dbname+"sslmode=disable") // "prod"
     dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
     dbmap.TraceOn("query:", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 
