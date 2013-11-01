7c750b95cc32668cd0f215b8f8e0f6c1d9ec5cac
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-31 22:52:20 -0400

    remove commented out code for connecting to postgres

--- db/dbsetup.go
+++ db/dbsetup.go
@@ -8,19 +8,11 @@ import (
     "log"
 )
 
-// func CreatePgSpec(key string, heroku_env_var string) string {
-//     return key+"="+os.Getenv(heroku_env_var)+" "
-// }
-
 func DbSetup(m map[string]interface{}) (dbmap *gorp.DbMap) {
-    // user := CreatePgSpec("user", os.Getenv("HEROKU_POSTGRESQL_USER"))
-    // pass := CreatePgSpec("password", os.Getenv("HEROKU_POSTGRESQL_PASS"))
-    // dbname := CreatePgSpec("dbname", os.Getenv("HEROKU_POSTGRESQL_DBNAME"))
-    // host := CreatePgSpec("host", os.Getenv("HEROKU_POSTGRESQL_HOST"))
     url := os.Getenv("HEROKU_POSTGRESQL_VIOLET_URL")
     connection, _ := pq.ParseURL(url)
     connection += " sslmode=require"
-    db, _ := sql.Open("postgres", connection) // "prod"
+    db, _ := sql.Open("postgres", connection)
     dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
     dbmap.TraceOn("query:", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 
