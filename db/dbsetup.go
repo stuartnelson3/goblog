8f243bef9454f5d2ec3f435eb78030bd169d8f3a
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-31 21:20:51 -0400

    adding pg credentials for clever cloud

--- db/dbsetup.go
+++ db/dbsetup.go
@@ -9,7 +9,8 @@ import (
 )
 
 func DbSetup(m map[string]interface{}) (dbmap *gorp.DbMap) {
-    db, _ := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable")
+    // db, _ := sql.Open("postgres", "user=stuartnelson dbname=goblog sslmode=disable") // dev
+    db, _ := sql.Open("postgres", "user=u6aq2gkmaiir3hrv password=56afebf98a164079989849adc9d589d1 host=bcvpvcjrov5cbmt6.postgresql.clvrcld.net dbname=bcvpvcjrov5cbmt6 sslmode=disable") // "prod"
     dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
     dbmap.TraceOn("query:", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 
