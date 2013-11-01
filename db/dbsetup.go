561d5e2d87831b78d3269daf4bc36131e9c2a714
Stuart Nelson <stuartnelson3@gmail.com>
2013-10-31 22:39:04 -0400

    try with pg parsing the pg url from heroku

--- db/dbsetup.go
+++ db/dbsetup.go
@@ -2,22 +2,25 @@ package dbsetup
 
 import (
     "github.com/coopernurse/gorp"
-    _ "github.com/bmizerany/pq"
+    "github.com/bmizerany/pq"
     "database/sql"
     "os"
     "log"
 )
 
-func CreatePgSpec(key string, heroku_env_var string) string {
-    return key+"="+os.Getenv(heroku_env_var)+" "
-}
+// func CreatePgSpec(key string, heroku_env_var string) string {
+//     return key+"="+os.Getenv(heroku_env_var)+" "
+// }
 
 func DbSetup(m map[string]interface{}) (dbmap *gorp.DbMap) {
-    user := CreatePgSpec("user", os.Getenv("HEROKU_POSTGRESQL_USER"))
-    pass := CreatePgSpec("password", os.Getenv("HEROKU_POSTGRESQL_PASS"))
-    dbname := CreatePgSpec("dbname", os.Getenv("HEROKU_POSTGRESQL_DBNAME"))
-    host := CreatePgSpec("host", os.Getenv("HEROKU_POSTGRESQL_HOST"))
-    db, _ := sql.Open("postgres", user+pass+host+dbname) // "prod"
+    // user := CreatePgSpec("user", os.Getenv("HEROKU_POSTGRESQL_USER"))
+    // pass := CreatePgSpec("password", os.Getenv("HEROKU_POSTGRESQL_PASS"))
+    // dbname := CreatePgSpec("dbname", os.Getenv("HEROKU_POSTGRESQL_DBNAME"))
+    // host := CreatePgSpec("host", os.Getenv("HEROKU_POSTGRESQL_HOST"))
+    url := os.Getenv("HEROKU_POSTGRESQL_VIOLET_URL")
+    connection, _ := pq.ParseURL(url)
+    connection += " sslmode=require"
+    db, _ := sql.Open("postgres", connection) // "prod"
     dbmap = &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
     dbmap.TraceOn("query:", log.New(os.Stdout, "myapp:", log.Lmicroseconds))
 
