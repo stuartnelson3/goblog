e41114e1ba863af9cbf246c17628c9ceb2aa46b8
Stuart Nelson <stuartnelson3@gmail.com>
2013-11-04 23:05:45 -0500

    go source to configure environment variables on heroku

---
+++ setup_env_vars.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+    "os/exec"
+    "bytes"
+    "fmt"
+    "crypto/sha512"
+    "io"
+    "encoding/hex"
+    "time"
+)
+
+func main() {
+    var envVar, app, input string
+    var out bytes.Buffer
+
+    GetAppName(&app, &out)
+    GetVariable(&envVar)
+    GetValueToHash(&input, envVar)
+
+    h := sha512.New()
+    io.WriteString(h, input)
+    hashedValue := hex.EncodeToString(h.Sum(nil))
+
+    SetEnvVar(envVar, hashedValue, app, &out)
+}
+
+func GetValueToHash(input *string, envVar string) {
+    fmt.Printf("Enter the value to be hashed for %s\n", envVar)
+    fmt.Scanln(input)
+}
+
+func GetVariable(env *string) {
+    fmt.Println("Enter the environment variable to set:")
+    fmt.Println("(options: HASHED_PASSWORD, HASHED_USERNAME, BLOGTOKEN")
+    fmt.Scanln(env)
+}
+
+func GetAppName(app *string, out *bytes.Buffer) {
+    fmt.Printf("What is the name of your app?\n")
+    time.Sleep(time.Millisecond * 500)
+    fmt.Printf("Here is the available list:\n")
+
+    cmd := exec.Command("heroku", "apps")
+    cmd.Stdout = out
+    cmd.Stderr = out
+    cmd.Run()
+    fmt.Println(out.String())
+    out.Reset()
+
+    fmt.Scanln(app)
+}
+
+func SetEnvVar(envVar string, hashedValue string, app string, out *bytes.Buffer) {
+    cmd := exec.Command("heroku", "config:set", envVar+"="+hashedValue, "--app", app)
+    cmd.Stdout = out
+    cmd.Stderr = out
+    cmd.Run()
+    fmt.Println(out.String())
+}
