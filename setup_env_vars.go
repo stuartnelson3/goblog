d01e7d7df393df3d11714118fbf67c57cd357db4
Stuart Nelson <stuartnelson3@gmail.com>
2013-11-05 15:24:14 -0500

    setup_env_vars executable goes through all of your env vars each time

--- setup_env_vars.go
+++ setup_env_vars.go
@@ -8,53 +8,59 @@ import (
     "io"
     "encoding/hex"
     "time"
+    "bufio"
+    "os"
 )
 
 func main() {
-    var envVar, app, input string
+    var app string
     var out bytes.Buffer
 
-    GetAppName(&app, &out)
-    GetVariable(&envVar)
-    GetValueToHash(&input, envVar)
+    envVariables :=
+        map[string]string{"BLOGTOKEN":"", "HASHED_USERNAME":"", "HASHED_PASSWORD":""}
 
-    h := sha512.New()
-    io.WriteString(h, input)
-    hashedValue := hex.EncodeToString(h.Sum(nil))
+    GetAppName(&app, out)
 
-    SetEnvVar(envVar, hashedValue, app, &out)
+    envVariables = GetEnvVariables(envVariables)
+
+    for envVar, value := range envVariables {
+        h := sha512.New()
+        io.WriteString(h, value)
+        hashedValue := hex.EncodeToString(h.Sum(nil))
+        SetEnvVar(envVar, hashedValue, app, out)
+    }
+
+    fmt.Println("Finished setting up environment variables.")
 }
 
-func GetValueToHash(input *string, envVar string) {
-    fmt.Printf("Enter the value to be hashed for %s\n", envVar)
-    fmt.Scanln(input)
+func GetEnvVariables(envVariables map[string]string) map[string]string {
+    scanner := bufio.NewScanner(os.Stdin)
+    for envVar, _ := range envVariables {
+        fmt.Printf("Enter the value to be hashed for %s\n", envVar)
+        scanner.Scan()
+        envVariables[envVar] = scanner.Text()
+    }
+    return envVariables
 }
 
-func GetVariable(env *string) {
-    fmt.Println("Enter the environment variable to set:")
-    fmt.Println("(options: HASHED_PASSWORD, HASHED_USERNAME, BLOGTOKEN")
-    fmt.Scanln(env)
-}
-
-func GetAppName(app *string, out *bytes.Buffer) {
+func GetAppName(app *string, out bytes.Buffer) {
     fmt.Printf("\nPick your app from the list of available apps:\n")
     time.Sleep(time.Millisecond * 500)
 
     cmd := exec.Command("heroku", "apps")
-    cmd.Stdout = out
-    cmd.Stderr = out
+    cmd.Stdout = &out
+    cmd.Stderr = &out
     cmd.Run()
     fmt.Println(out.String())
-    out.Reset()
 
     fmt.Printf("What is the name of your app?\n")
     fmt.Scanln(app)
 }
 
-func SetEnvVar(envVar string, hashedValue string, app string, out *bytes.Buffer) {
+func SetEnvVar(envVar string, hashedValue string, app string, out bytes.Buffer) {
     cmd := exec.Command("heroku", "config:set", envVar+"="+hashedValue, "--app", app)
-    cmd.Stdout = out
-    cmd.Stderr = out
+    cmd.Stdout = &out
+    cmd.Stderr = &out
     cmd.Run()
     fmt.Println(out.String())
 }
