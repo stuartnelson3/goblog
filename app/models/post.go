bde3b6ca30583293e20e6191de89d014fc7a08f1
Stuart Nelson <stuartnelson3@gmail.com>
2013-12-05 17:14:20 -0500

    modtime wasnt working, save unix time of post creation and sort by that

--- app/models/post.go
+++ app/models/post.go
@@ -10,7 +10,6 @@ import (
     "strings"
     "regexp"
     "github.com/russross/blackfriday"
-    "os"
     "sort"
 )
 
@@ -19,29 +18,27 @@ type Post struct {
     Body      string `json:"body"`
     Slug      string `json:"slug"`
     CreatedAt string `json:"createdAt"`
-    Mtime     string `json:"mtime"`
+    Mtime     int64  `json:"mtime"`
 }
 
 // sort posts by mtime
-type ByMtime []string
+type ByMtime []*Post
 func (a ByMtime) Len() int           { return len(a) }
 func (a ByMtime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a ByMtime) Less(i, j int) bool {
-    ai, _ := os.Stat(a[i])
-    aj, _ := os.Stat(a[j])
-    return ai.ModTime().After(aj.ModTime())
+    return a[i].Mtime > a[j].Mtime
 }
 
 func (p Post) All() []*Post {
     var posts []*Post
     matches, _ := filepath.Glob("app/views/Posts/*.json")
-    sort.Sort(ByMtime(matches))
     for i:=0; i<len(matches); i++ {
         var post = &Post{}
         data, _ := ioutil.ReadFile(matches[i])
         json.Unmarshal(data, post)
         posts = append(posts, post)
     }
+    sort.Sort(ByMtime(posts))
     return posts
 }
 
@@ -57,8 +54,13 @@ func (p *Post) Create() error {
     p.ParseBody()
     p.CreateSlug()
     p.CreateTimestamp()
+    p.SetMtime()
     err := p.SaveJson()
     return err
+}
+
+func (p *Post) SetMtime() {
+    p.Mtime = time.Now().Unix()
 }
 
 func (p *Post) SaveJson() error {
